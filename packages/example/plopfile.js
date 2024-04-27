652f7fee564af33dbc93192d6da0acb55a9f47bb
zone <purecanee@gmail.com>
2024-04-27 16:30:28 +0800

    feat: asd

--- packages/example/plopfile.js
+++ packages/example/plopfile.js
@@ -1,32 +1,35 @@
-export default async (plop) => {
-  await plop.load('client-api-plop');
+module.exports = async (
+  /** @type {import('plop').NodePlopAPI} */
+  plop,
+) => {
+  await plop.load('plop-action-eslint');
 
-  // plop.setGenerator('Template', {
-  //   description: 'Use Template',
-  //   prompts: [
-  //     {
-  //       type: 'input',
-  //       name: 'name',
-  //     },
-  //   ],
-  //   actions: [
-  //     {
-  //       type: 'add',
-  //       path: 'lib/{{pascalCase name}}.ts',
-  //       templateFile: 'templates/index.ts',
-  //       skipIfExists: true,
-  //     },
-  //     {
-  //       type: 'modify',
-  //       path: 'lib/{{pascalCase name}}.ts',
-  //       pattern: /REPLACEME/g,
-  //       template: '{{pascalCase name}}',
-  //     },
-  //     {
-  //       // @ts-expect-error TODO: improve typing
-  //       type: 'eslint',
-  //       path: 'lib/{{pascalCase name}}.ts',
-  //     },
-  //   ],
-  // });
+  plop.setGenerator('Template', {
+    description: 'Use Template',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'lib/{{pascalCase name}}.ts',
+        templateFile: 'templates/index.ts',
+        skipIfExists: true,
+      },
+      {
+        type: 'modify',
+        path: 'lib/{{pascalCase name}}.ts',
+        pattern: /REPLACEME/g,
+        template: '{{pascalCase name}}',
+      },
+      {
+        // @ts-expect-error TODO: improve typing
+        type: 'eslint',
+        path: 'lib/{{pascalCase name}}.ts',
+      },
+    ],
+  });
 };
